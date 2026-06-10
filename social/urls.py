e81cc338e3214942c06a9e6109eb0b3cbd0c1a5e
ShahabCode <shahabepic@gmail.com>
2026-06-10 14:16:42 +0330

    feat: add password change and email-based password reset

--- social/urls.py
+++ social/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('user/edit/', views.edit_user, name='edit_account'),
     path('ticket/', views.ticket, name='ticket'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(success_url='done'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(success_url='/password_reset/done/'), name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url='/password_reset/complete'), name="password_reset_confirm"),
+    path('password_reset/complete/',auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
